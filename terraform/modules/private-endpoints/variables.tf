ec278ec823d389740ef2dd75e44c7361e93018c6
LetItGo90 <austinmundy90@gmail.com>
2026-05-14 09:25:47 -0500

    dfs

--- terraform/modules/private-endpoints/variables.tf
+++ terraform/modules/private-endpoints/variables.tf
@@ -1,32 +1,14 @@
 variable "resource_group_name" {
   type = string
 }
-
 variable "location" {
   type = string
-}
-
-variable "vnet_hub_name" {
-  type    = string
-  default = "vnet-hub"
-}
-variable "hub_vnet_address_space" {
-  type = list(string)
 }
 
 variable "hub_vnet_id" {
   type = string
 }
 
-variable "spoke_vnet_address_space" {
-  type    = list(string)
-  default = ["10.1.0.0/16"]
-}
-
-variable "vnet_spoke_name" {
-  type    = string
-  default = "vnet-spoke"
-}
 
 variable "key_vault_id" {
   type = string
